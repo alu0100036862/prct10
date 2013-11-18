79390abda39c980a35b70805f6619cb0449084bd
Dailos Herrera Bencomo <alu0100036862@etsii.ull.es>
2013-11-18 19:32:07 +0000

    creamos estrutura de clases e implementamos la clase matrizDensa de la practica anterior

---
+++ lib/modai_prct10/matrizAbstracta.rb
@@ -0,0 +1,55 @@
+!# /usr/bin/ruby
+# matrizAbstracta.rb
+require "racional"
+
+# Clase abstracta para herencia común de matrices densa y dispersa
+class MatrizAbstracta
+
+	# Inicialización
+	def initialize()
+
+		@filas = 0 
+		@columnas = 0
+
+	end
+
+	attr_reader :filas, :columnas
+
+	# Convertimos a string
+	def to_s
+
+	end
+
+	#  Matriz en punto flotante
+	def to_f
+
+	end
+
+	# Suma de matrices
+	def +(o)
+
+	end
+
+	# Resta de matrices
+	def -(o)
+
+	end
+
+	# Multiplicación de matrices
+	def *(o)
+
+	end
+
+	# Máximo de matriz
+	def max(o)
+
+	end
+
+	# Minimo de matriz
+	def min(o)
+
+	end
+
+end
+
+
